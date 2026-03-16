8a03c4f01c718e980d1e7a325d2c1ab723c94695
Sharath Chandra Reddy Balaiahgari <tripmate937@gmail.com>
2026-03-16 12:00:00 +0530

    Added Application class for initialising

--- app/src/main/java/uk/ac/tees/mad/tripmate/TripMateApplication.kt
+++ app/src/main/java/uk/ac/tees/mad/tripmate/TripMateApplication.kt
@@ -1,5 +1,12 @@
 package uk.ac.tees.mad.tripmate
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 
+class TripMateApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
 
