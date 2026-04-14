875c167db984eba78f7b0e9ddd4f600e42c6c6ae
Sharath Chandra Reddy Balaiahgari <tripmate937@gmail.com>
2026-04-14 12:00:00 +0530

    Created Trip Mapper

--- app/src/main/java/uk/ac/tees/mad/tripmate/data/local/TripDatabase.kt
+++ app/src/main/java/uk/ac/tees/mad/tripmate/data/local/TripDatabase.kt
@@ -1,2 +1,35 @@
 package uk.ac.tees.mad.tripmate.data.local
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [TripEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TripDatabase : RoomDatabase() {
+
+    abstract fun tripDao(): TripDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TripDatabase? = null
+
+        fun getDatabase(context: Context): TripDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TripDatabase::class.java,
+                    "trip_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
